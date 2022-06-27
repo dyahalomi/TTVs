2f544ddbc5572fb992d4cec53b4303622b509ed3
dyahalomi <danielyahalomi@gmail.com>
2022-06-27 15:36:30 -0400

    by Diana inputs to match problem_times_only.py

--- detrend_package/detrend_lc.py
+++ detrend_package/detrend_lc.py
@@ -1,4 +1,3 @@
-
 from datetime import date
 import numpy as np
 import pandas as pd
@@ -14,23 +13,23 @@ import ast
 
 warnings.simplefilter('ignore', np.RankWarning)
 
-parser = argparse.ArgumentParser(description="Looks up light curves for TESS and Kepler objects and enables labeling of jump times. \
-	Detrends light curves, outputs plots, and saves detrended data as csvs.",
+parser = argparse.ArgumentParser(description="Looks up light curves for TESS and Kepler objects and enables labeling of jump times.",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('object', type=str, help='TESS or Kepler identifier. ex: "toi-2088"')
-parser.add_argument('flux_type', type=str, help='Flux type as a string. Options: pdc, sap, or both')
-parser.add_argument('mission', type=str, help='Mission data select. ex: "TESS"')
-parser.add_argument('planet_num', type=int, help='Which planet to look at in system. ex: 1')
-parser.add_argument('save_to_dir', type=str, help='Directory path to save csvs and figures to.')
+parser.add_argument('--flux_type', type=str, default='sap', help='Flux type as a string. Options: pdc, sap, both, or qlp.')
+parser.add_argument('--mission', type=str, default='TESS', help='Mission data select. ex: "TESS"')
+parser.add_argument('--planet_num', type=int, default=1, help='Which planet to look at in system. ex: 1')
+parser.add_argument('--save_to_dir', type=str, default='./', help='Directory path to save csvs and figures to.')
 parser.add_argument('-d', '--depth', default=0.02, help='Sets depth of detrended plots. Default is 0.02.')
 parser.add_argument('-p', '--period', default=None, help='Optionally input period. Otherwise defaults to what \
-	Exoplanet Archive can find.')
+    Exoplanet Archive can find.')
 parser.add_argument('-t', '--t0', default=None, help='Optionally input t0. Otherwise defaults to what Exoplanet Archive can find.')
 parser.add_argument('-du', '--duration', default=None, help='Optionally input duration. Otherwise defaults to what \
-	Exoplanet Archive can find.')
+    Exoplanet Archive can find.')
 parser.add_argument('-mw', '--mask_width', default=1.3, help='Sets mask width. Default is 1.3.')
 parser.add_argument('-s', '--show_plots', default='True', help='Set whether to show non-problem-time plots.')
 parser.add_argument('--dont_bin', default='False', help='if True, then jump time plots data wont be binned.')
+parser.add_argument('--save_transit_data', default='False', help='if True, then will save data on each transit')
 
 
 args = vars(parser.parse_args())
@@ -46,9 +45,10 @@ input_depth = args['depth']
 input_period = args['period']
 input_t0 = args['t0']
 input_duration = args['duration']
-input_mask_width = args['mask_width']
+input_mask_width = float(args['mask_width'])
 input_show_plots = ast.literal_eval(args['show_plots'])
 input_dont_bin = ast.literal_eval(args['dont_bin'])
+input_save_transit_data = ast.literal_eval(args['save_transit_data'])
 
 
 
@@ -58,7 +58,7 @@ input_dont_bin = ast.literal_eval(args['dont_bin'])
 
 
 
-print(f"exoplanet.__version__ = '{xo.__version__}'")
+#print(f"exoplanet.__version__ = '{xo.__version__}'")
 
 from find_flux_jumps import *
 from get_lc import *
@@ -71,22 +71,28 @@ from detrend import *
 
 # checks user arguments
 if mission_select == 'TESS': 
-	tess_bool = True
-	kepler_bool = False
+    tess_bool = True
+    kepler_bool = False
 else:
-	tess_bool = False # assuming Kepler is selected
-	kepler_bool = True
+    tess_bool = False # assuming Kepler is selected
+    kepler_bool = True
 
 
-# determining figname
-# determining today's date
-today = date.today()
-current_day = today.strftime('%b%d_%Y')
+if input_dir == './':
 
-foldername = input_id + '.0' + str(input_planet_number)+ '_' + current_day
-path = os.path.join(input_dir, foldername)
+    # determining figname
+    # determining today's date
+    today = date.today()
+    current_day = today.strftime('%b%d_%Y')
 
-os.makedirs(path, exist_ok=True)
+    foldername = input_id + '/' + input_id + '.0' + str(input_planet_number) + '_' + current_day
+    path = os.path.join(input_dir, foldername)
+
+    os.makedirs(path, exist_ok=True)
+
+else:
+
+    path = input_dir
 
 
 # check if we should run both pdc and sap flux
@@ -104,6 +110,8 @@ if flux_type == 'both':
 
 
     # now for detrending!
+    print('Detrending now.')
+    
     [[sap_local_x, sap_local_y, sap_local_yerr, sap_local_mask, sap_local_mask_fitted_planet, \
     sap_local_detrended, sap_local_x_no_outliers, sap_local_detrended_no_outliers, \
     sap_poly_detrended, sap_poly_DWs, sap_poly_x_no_outliers, sap_poly_detrended_no_outliers, \
